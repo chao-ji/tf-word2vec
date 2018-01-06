b514264a2d85e0d3b03c99d6205cd18b12cc254c
chao-ji <jic@indiana.edu>
2018-01-06 23:03:56 +0800

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -2,9 +2,11 @@ import heapq
 import itertools
 import numpy as np
 import tensorflow as tf
+from collections import defaultdict
 from datetime import datetime
 
 class VocabWord(object):
+  """Stores attributes (`count`, `index`, `keep_prob`, ...) for each word in vocabulary"""
   def __init__(self, **kwargs):
     self.__dict__.update(kwargs)
 
@@ -24,33 +26,38 @@ class VocabWord(object):
 
 
 class Word2Vec(object):
+  """Trains word2vec model. The model can be trained on two model architectures 
+  "skip-gram" (`sg`) or "continuous bag-of-words" (`cbow`) and by two training algorithms 
+  "negative sampling" (`ns`) or "hierarchical softmax" (`hs`). 
+  """
+
   __slots__ = ("max_vocab_size", "min_count", "sample", "sorted_vocab", "window", "size",
     "norm_embeddings", "negatives", "power", "alpha", "min_alpha", "max_batch_size", "epochs",
     "log_every_n_steps", "hidden_layer_toggle", "output_layer_toggle", "ns_add_bias",
     "hs_add_bias", "clip_gradient", "seed", "_random_state", "_raw_vocab", "_unigram_count",
-    "vocab", "vocabulary_size", "index2word", "index2word", "num_words", "total_sents", "_syn0",
+    "vocab", "vocabulary_size", "index2word", "num_words", "total_sents", "_syn0",
     "_syn1", "_biases", "_progress", "_sents_covered")
 
   def __init__(self,
-                max_vocab_size=None,
-                min_count=5,
-                sample=1e-3,
-                sorted_vocab=True,                
-                window=5,
-                size=100,
-                norm_embeddings=False,
-                negatives=5,
-                power=0.75,
-                alpha=0.025,
-                min_alpha=0.0001,
-                max_batch_size=64,
-                epochs=5,
-                log_every_n_steps=10000,
-                hidden_layer_toggle=True,
-                output_layer_toggle=True,
-                ns_add_bias=True,
-                hs_add_bias=True,
-                clip_gradient=False,
+                max_vocab_size=None,      # Maximum vocabulary size
+                min_count=5,              # Minimum word count to be considered as a vocabulary word
+                sample=1e-3,              # Sub-sampling rate
+                sorted_vocab=True,        # Sort the vocabulary in descending order of word count
+                window=5,                 # Maximum number of words on either side of a window
+                size=100,                 # Embedding size
+                norm_embeddings=False,    # Normalize word embeddings to unit norm
+                negatives=5,              # Number of negative words for `ns`
+                power=0.75,               # Distortion of the sampling algorithm for `ns`
+                alpha=0.025,              # Initial learning rate
+                min_alpha=0.0001,         # Minimum learning rate
+                max_batch_size=64,        # Maximum mini-batch size
+                epochs=5,                 # Number of runs over the entire corpus
+                log_every_n_steps=10000,  # Prints out average loss in every N steps (mini-batches)
+                hidden_layer_toggle=True, # `True` for `sg`, `False` for `cbow`
+                output_layer_toggle=True, # `True` for `ns`, `False` for `hs`
+                ns_add_bias=True,         # Add bias to the logit in logistic regression for `ns`
+                hs_add_bias=True,         # Add bias to the logit in losistic regression for `hs`
+                clip_gradient=False,      # Clip the gradient in case of gradient explosion
                 seed=1):
     self.max_vocab_size = max_vocab_size
     self.min_count = min_count
@@ -78,13 +85,14 @@ class Word2Vec(object):
     self._sents_covered = 0
 
   def build_vocab(self, sents):
+    """Builds vocabulary in a one-pass-run of the corpus"""
     num_words = 0
     raw_vocab = self._get_raw_vocab(sents)
     vocab = dict()
     index2word = []
     for word, count in raw_vocab.iteritems():
       if count >= self.min_count:
-        vocab[word] = VocabWord(count=count, index=len(index2word)) # {"count": count, "index": len(index2word)}
+        vocab[word] = VocabWord(count=count, index=len(index2word))
         index2word.append(word)
         num_words += count
 
@@ -111,11 +119,11 @@ class Word2Vec(object):
     self.total_sents = len(sents) * self.epochs
 
   def _get_raw_vocab(self, sents):
-    raw_vocab = dict()
+    raw_vocab = defaultdict(int)
     word_count_cutoff = 1
     for sent in sents:
       for word in sent:
-        raw_vocab[word] = raw_vocab[word] + 1 if word in raw_vocab else 1
+        raw_vocab[word] += 1
       if self.max_vocab_size and len(raw_vocab) > self.max_vocab_size:
         for word in raw_vocab.keys():
           if raw_vocab[word] < word_count_cutoff:
@@ -126,41 +134,43 @@ class Word2Vec(object):
   def _get_tarcon_generator(self, sents_iter):
     return (tarcon for sent in sents_iter for tarcon in self._tarcon_per_sent(sent)) 
 
+  def _sg_ns(self, batch):
+    return np.array(batch[0]), np.array(batch[1])
+
+  def _cbow_ns(self, batch):
+    segment_ids = np.repeat(xrange(len(batch[0])), map(len, batch[1]))
+    return np.array([np.concatenate(batch[1]), segment_ids]).T, np.array(batch[0])
+
+  def _sg_hs(self, batch):
+    paths = [np.array([self.vocab[self.index2word[i]].point, self.vocab[self.index2word[i]].code]).T for i in batch[1]]
+    code_lengths = map(len, paths)
+    labels = np.vstack(paths)
+    inputs = np.repeat(batch[0], code_lengths)
+    return inputs, labels
+
+  def _cbow_hs(self, batch):
+    paths = [np.array([self.vocab[self.index2word[i]].point, self.vocab[self.index2word[i]].code]).T for i in batch[0]]
+    code_lengths = map(len, paths)
+    labels = np.vstack(paths)
+    contexts_repeated = np.repeat(batch[1], code_lengths, axis=0)
+    contexts_repeated_segment_ids = np.repeat(xrange(len(contexts_repeated)), map(len, contexts_repeated))
+    inputs = np.array([np.concatenate(contexts_repeated), contexts_repeated_segment_ids]).T
+    return inputs, labels
+
   def generate_batch(self, sents_iter):
-    vocab, index2word = self.vocab, self.index2word
-
-    def _sg_ns(batch):
-      return np.array(batch[0]), np.array(batch[1])
-    def _cbow_ns(batch):
-      segment_ids = np.repeat(xrange(len(batch[0])), map(len, batch[1]))
-      return np.array([np.concatenate(batch[1]), segment_ids]).T, np.array(batch[0])
-    def _sg_hs(batch):
-      paths = [np.array([vocab[index2word[i]].point, vocab[index2word[i]].code]).T for i in batch[1]]
-      code_lengths = map(len, paths)
-      labels = np.vstack(paths)
-      inputs = np.repeat(batch[0], code_lengths)
-      return inputs, labels
-    def _cbow_hs(batch):
-      paths = [np.array([vocab[index2word[i]].point, vocab[index2word[i]].code]).T for i in batch[0]] 
-      code_lengths = map(len, paths)
-      labels = np.vstack(paths)
-      contexts_repeated = np.repeat(batch[1], code_lengths)
-      contexts_repeated_segment_ids = np.repeat(xrange(len(contexts_repeated)), map(len, contexts_repeated))
-      inputs = np.array([np.concatenate(contexts_repeated), contexts_repeated_segment_ids]).T
-      return inputs, labels
-
+    """Generates word indices in a mini-batch""" 
     def _yield_fn(batch):
       hidden_toggle = self.hidden_layer_toggle
       out_toggle = self.output_layer_toggle
 
       if hidden_toggle and out_toggle:
-        return _sg_ns(batch)
+        return self._sg_ns(batch)
       elif (not hidden_toggle) and out_toggle:
-        return _cbow_ns(batch)
+        return self._cbow_ns(batch)
       elif hidden_toggle and (not out_toggle):
-        return _sg_hs(batch)
+        return self._sg_hs(batch)
       elif (not hidden_toggle) and (not out_toggle):
-        return _cbow_hs(batch)
+        return self._cbow_hs(batch)
 
     tarcon_generator = self._get_tarcon_generator(sents_iter) 
 
@@ -179,7 +189,6 @@ class Word2Vec(object):
       batch = []
 
   def _keep_word(self, word):
-    """Determine if input word will be kept."""
     return word in self.vocab and self._random_state.binomial(1, self.vocab[word].keep_prob)
 
   def _tarcon_per_target(self, sent_trimmed, word_index):
@@ -199,7 +208,6 @@ class Word2Vec(object):
         yield target, contexts
 
   def _tarcon_per_sent(self, sent):
-    """Generator: yields 2-tuples of tar(get) and con(text) words per sentences."""
     sent_trimmed = [self.vocab[word].index for word in sent if self._keep_word(word)]
 
     for word_index in xrange(len(sent_trimmed)):
@@ -210,6 +218,7 @@ class Word2Vec(object):
     self._progress = self._sents_covered / float(self.total_sents)
 
   def create_binary_tree(self):
+    """Builds huffmann tree based on the frequencies of all vocabulary words"""
     vocab = self.vocab
     heap = list(vocab.itervalues())
     heapq.heapify(heap)
@@ -233,6 +242,7 @@ class Word2Vec(object):
     return (random.rand(self.size) - 0.5) / self.size
 
   def initialize_variables(self):
+    """Defines `tf.Variable` and `tf.placeholfer`"""
     syn0_val = np.empty((self.vocabulary_size, self.size), dtype=np.float32)
     for i in xrange(self.vocabulary_size):
       syn0_val[i] = self._seeded_vector(self.index2word[i] + str(self.seed))
@@ -241,8 +251,8 @@ class Word2Vec(object):
     self._syn1 = tf.Variable(tf.truncated_normal([self.vocabulary_size, self.size],
                                 stddev=1.0/np.sqrt(self.size)), dtype=tf.float32)
     self._biases = tf.Variable(tf.zeros([self.vocabulary_size]), dtype=tf.float32)
-    inputs = tf.placeholder(dtype=tf.int64, shape=[None] if self.hidden_layer_toggle else [None, 2])
-    labels = tf.placeholder(dtype=tf.int64, shape=[None] if self.output_layer_toggle else [None, 2])
+    inputs = tf.placeholder(dtype=tf.int64)
+    labels = tf.placeholder(dtype=tf.int64)
     return inputs, labels
 
   def _input_to_hidden(self, syn0, inputs):
@@ -252,6 +262,8 @@ class Word2Vec(object):
       return tf.segment_mean(tf.nn.embedding_lookup(syn0, inputs[:, 0]), inputs[:, 1])
   
   def loss_ns(self, inputs, labels):
+    """Loss for negative sampling (`ns`)
+    V=vocab_size, D=embed_size, N=batch_size, K=negative_samples"""
     # [V, D], [V, D]
     syn0, syn1 = self._syn0, self._syn1
     sampled_values = tf.nn.fixed_unigram_candidate_sampler(
@@ -292,6 +304,8 @@ class Word2Vec(object):
     return loss
 
   def loss_hs(self, inputs, labels):
+    """Loss for hierarchical sampling (`hs`) 
+    V=vocab_size, D=embed_size"""
     # [V, D], [V, D]
     syn0, syn1 = self._syn0, self._syn1
     # [SUM(CODE_LENGTHS), D]
@@ -325,6 +339,15 @@ class Word2Vec(object):
       return sgd.minimize(loss)
 
   def train(self, sents, sess):
+    """Trains word2vec model.
+
+    Args:
+      `sents`: an iterable of list of strings
+      `sess`: TensorFlow session
+
+    Returns:
+      `WordEmbeddings` instance
+    """
     self.build_vocab(sents)
     if not self.output_layer_toggle:
       self.create_binary_tree()
@@ -367,6 +390,9 @@ class Word2Vec(object):
 
 
 class WordEmbeddings(object):
+  """Trained word2vec model. Contains the index-to-word mapping, vocabulary and 
+  final word embeddings"""
+
   def __init__(self, syn0_final, vocab, index2word):
     self.syn0_final = syn0_final
     self.vocab = vocab
@@ -379,6 +405,7 @@ class WordEmbeddings(object):
     return self.syn0_final[self.vocab[word].index]
 
   def most_similar(self, word, k):
+    """Finds the top-k words with smallest cosine distances w.r.t `word`"""
     if word not in self.vocab:
       raise ValueError("Word '%s' not found in the vocabulary" % word)
     if k >= self.syn0_final.shape[0]:
