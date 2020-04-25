use tf.gradients instead of gradient tape as a workaround to bypass the internal bug for gradient computing

tf2.x/model.py
@@ -98,7 +98,6 @@ class Word2VecModel(tf.keras.Model):
     Returns:
       loss: float tensor of shape [batch_size, sample_size + 1].
     """
-
     syn0, syn1, biases = self.weights
 
     sampled_values = tf.random.fixed_unigram_candidate_sampler(
@@ -116,7 +115,6 @@ class Word2VecModel(tf.keras.Model):
     true_syn1 = tf.gather(syn1, labels) # [N, D]
     sampled_syn1 = tf.gather(syn1, sampled_mat) # [N, K, D]
     true_logits = tf.reduce_sum(tf.multiply(inputs_syn0, true_syn1), 1) # [N]
-
     sampled_logits = tf.einsum('ijk,ikl->il', tf.expand_dims(inputs_syn0, 1), 
         tf.transpose(sampled_syn1, (0, 2, 1)))
 
@@ -144,11 +142,16 @@ class Word2VecModel(tf.keras.Model):
       loss: float tensor of shape [sum_of_code_len]
     """
     syn0, syn1, biases = self.weights
-    max_depth = (labels.shape.as_list()[1] - 1) // 2
 
-    def func(args):
-      inputs_syn0, codes_points = args
+    inputs_syn0_list = tf.unstack(self._get_inputs_syn0(inputs))
+    codes_points_list = tf.unstack(labels)
+    max_depth = (labels.shape.as_list()[1] - 1) // 2
+    loss = []
+    for i in range(self._batch_size):
+      inputs_syn0 = inputs_syn0_list[i]
+      codes_points = codes_points_list[i]
       true_size = codes_points[-1]
+
       codes = codes_points[:true_size]
       points = codes_points[max_depth:max_depth+true_size]
 
@@ -157,20 +160,9 @@ class Word2VecModel(tf.keras.Model):
       if self._add_bias:
         logits += tf.gather(biases, points)
 
-      losses = tf.nn.sigmoid_cross_entropy_with_logits(
-          labels=tf.cast(codes, 'float32'), logits=logits)
-      loss = tf.reduce_sum(losses)
-      count = tf.size(losses)
-
-      loss = tf.reshape(loss, (1,)) 
-      count = tf.reshape(count, (1,))
-      result = tf.concat([tf.cast(loss, 'float32'), tf.cast(count, 'float32')], axis=0)
-      return result
-
-    result = tf.map_fn(func, (self._get_inputs_syn0(inputs), labels), 'float32')
-    result = tf.reduce_sum(result, axis=0)
-    loss = result[0] / result[1]
-
+      loss.append(tf.nn.sigmoid_cross_entropy_with_logits(
+          labels=tf.cast(codes, 'float32'), logits=logits))
+    loss = tf.concat(loss, axis=0)
     return loss
 
   def _get_inputs_syn0(self, inputs):
@@ -188,13 +180,14 @@ class Word2VecModel(tf.keras.Model):
     if self._arch == 'skip_gram':
       inputs_syn0 = tf.gather(syn0, inputs)
     else:
-
-      def func(contexts):
+      inputs_syn0 = []
+      contexts_list = tf.unstack(inputs)
+      for i in range(self._batch_size):
+        contexts = contexts_list[i]
         context_words = contexts[:-1]
         true_size = contexts[-1]
-        result = tf.reduce_mean(tf.gather(syn0, context_words[:true_size]), axis=0)
-        return result
+        inputs_syn0.append(
+            tf.reduce_mean(tf.gather(syn0, context_words[:true_size]), axis=0))
+      inputs_syn0 = tf.stack(inputs_syn0)
 
-      inputs_syn0 = tf.map_fn(func, inputs, dtype='float32')
-      
     return inputs_syn0

tf2.x/run_training.py
@@ -34,7 +34,7 @@ flags.DEFINE_integer('max_vocab_size', 0, 'Maximum vocabulary size. If > 0, '
 flags.DEFINE_integer('min_count', 10, 'Words whose counts < `min_count` are not'
     ' included in the vocabulary.')
 flags.DEFINE_float('sample', 1e-3, 'Subsampling rate.')
-flags.DEFINE_integer('window_size', 10, 'Num of words on the left or right side' 
+flags.DEFINE_integer('window_size', 10, 'Num of words on the left or right side'
     ' of target word within a window.')
 
 flags.DEFINE_integer('hidden_size', 300, 'Length of word vector.')
@@ -47,7 +47,8 @@ flags.DEFINE_boolean('add_bias', True, 'Whether to add bias term to dotproduct '
 
 flags.DEFINE_integer('log_per_steps', 10000, 'Every `log_per_steps` steps to '
     ' output logs.')
-flags.DEFINE_list('filenames', None, 'Names of comma-separated input text files.')
+flags.DEFINE_list(
+    'filenames', None, 'Names of comma-separated input text files.')
 flags.DEFINE_string('out_dir', '/tmp/word2vec', 'Output directory.')
 
 FLAGS = flags.FLAGS
@@ -72,7 +73,8 @@ def main(_):
   filenames = FLAGS.filenames
   out_dir = FLAGS.out_dir
 
-  tokenizer = WordTokenizer(max_vocab_size=max_vocab_size, min_count=min_count, sample=sample)
+  tokenizer = WordTokenizer(
+      max_vocab_size=max_vocab_size, min_count=min_count, sample=sample)
   tokenizer.build_vocab(filenames)
 
   builder = Word2VecDatasetBuilder(tokenizer,
@@ -81,9 +83,7 @@ def main(_):
                                    epochs=epochs,
                                    batch_size=batch_size,
                                    window_size=window_size)
-
   dataset = builder.build_dataset(filenames)
-
   word2vec = Word2VecModel(tokenizer.unigram_counts,
                arch=arch,
                algm=algm,
@@ -95,18 +95,14 @@ def main(_):
                min_alpha=min_alpha,
                add_bias=add_bias)
 
-  train_step_signature = utils.get_train_step_signature(arch, algm, batch_size, window_size, builder._max_depth)
-
-
+  train_step_signature = utils.get_train_step_signature(
+      arch, algm, batch_size, window_size, builder._max_depth)
   optimizer = tf.keras.optimizers.SGD(1.0)
-
 
   @tf.function(input_signature=train_step_signature)
   def train_step(inputs, labels, progress):
-    with tf.GradientTape() as tape:
-      loss = word2vec(inputs, labels)
-
-    gradients = tape.gradient(loss, word2vec.trainable_variables)
+    loss = word2vec(inputs, labels)
+    gradients = tf.gradients(loss, word2vec.trainable_variables)
 
     learning_rate = tf.maximum(alpha * (1 - progress[0]) +
         min_alpha * progress[0], min_alpha)
@@ -125,7 +121,6 @@ def main(_):
       gradients[2]._values *= learning_rate
     else:
       gradients[2] *= learning_rate
-
 
     optimizer.apply_gradients(
         zip(gradients, word2vec.trainable_variables))
@@ -148,7 +143,8 @@ def main(_):
   with tf.io.gfile.GFile(os.path.join(FLAGS.out_dir, 'vocab.txt'), 'w') as f:
     for w in tokenizer.table_words:
       f.write(w + '\n')
-  print('Word embeddings saved to', os.path.join(FLAGS.out_dir, 'syn0_final.npy'))
+  print('Word embeddings saved to', 
+      os.path.join(FLAGS.out_dir, 'syn0_final.npy'))
   print('Vocabulary saved to', os.path.join(FLAGS.out_dir, 'vocab.txt'))
 
 